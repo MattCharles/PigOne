97be1f948778fd493c8489ec6ca02c24a49cec44
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-11-01 19:11:59 -0700

    Initial commit

---
+++ main_menu.gd
@@ -0,0 +1,41 @@
+extends Control
+
+@onready var start_label := $ButtonContainer/Start/Label
+@onready var options_label := $ButtonContainer/Options/Label
+@onready var exit_label := $ButtonContainer/Exit/Label
+@onready var oinker := $Oinker
+@export var PRESS_OFFSET := Vector2(0.0, 20.0)
+
+
+
+func move_button(label: Label, pressed:bool) -> void:
+	label.position = label.position + PRESS_OFFSET if pressed else label.position - PRESS_OFFSET
+
+
+func _on_start_button_down():
+	move_button(start_label, true)
+
+
+func _on_start_button_up():
+	move_button(start_label, false)
+
+
+func _on_options_button_down():
+	move_button(options_label, true)
+
+
+func _on_options_button_up():
+	move_button(options_label, false)
+
+
+func _on_exit_button_down():
+	get_tree().quit()
+	move_button(exit_label, true)
+
+
+func _on_exit_button_up():
+	move_button(exit_label, false)
+
+
+func _on_button_entered():
+	oinker.play()
