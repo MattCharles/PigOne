732ac650ab504af288f9cf841a08a441d12cc421
Matt Miller <matthew.c.anthony.miller@gmail.com>
2023-11-05 15:32:01 -0800

    Game can end

--- countdown.gd
+++ countdown.gd
@@ -7,11 +7,13 @@ var t:=0.0
 @onready var progress_label := $Label
 @export var cooldown := 0.1
 @export var gameover := false
+@onready var try_again := $"../RetryMenu"
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if progress_bar.value <= 0:
 		gameover = true
+		try_again.visible = true
 		return
 	t+=delta
 	if t >= cooldown:
---
+++ retry.gd
@@ -0,0 +1,14 @@
+extends Control
+@onready var yeah_label := $Control/Yeah/Label
+@export var PRESS_OFFSET := Vector2(0.0, 20.0)
+
+func move_button(label: Label, pressed:bool) -> void:
+	label.position = label.position + PRESS_OFFSET if pressed else label.position - PRESS_OFFSET
+
+
+func _on_yeah_button_down():
+	move_button(yeah_label, true)
+
+
+func _on_yeah_button_up():
+	move_button(yeah_label, false)
