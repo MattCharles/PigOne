62dd150f878423735814f2d06846daf80c192173
Warren Scipio <warrenscipio@gmail.com>
2023-11-06 13:38:24 -0800

    pig grows poorly

--- Scripts/yoyo.gd
+++ Scripts/yoyo.gd
@@ -7,3 +7,10 @@ var t := 0.0
 func _physics_process(delta):
 	t += delta
 	position.z = default_distance + sin(speed * t)
+	
+	
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				default_distance += .2
