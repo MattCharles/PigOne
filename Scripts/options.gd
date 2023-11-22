1c1af0311d23b485b75c8c353c6f04820e748ca4
RyanCross <ryan.cross.home@gmail.com>
2023-11-22 00:01:29 -0600

    Map sliders to audio buses

--- Scripts/options.gd
+++ Scripts/options.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 func _on_credits_pressed():
 	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
 
@@ -14,3 +15,12 @@ func _on_options_pressed():
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_sfx_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("SFX"), value)
+
+
+func _on_music_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), value)
+	
