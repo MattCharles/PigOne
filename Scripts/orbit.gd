3075a38dda07a6036aea3ac1252fa8c43f0144cf
Warren Scipio <warrenscipio@gmail.com>
2023-11-28 02:05:48 -0800

    made the pig growth exponential, removed position logic related to pig growth looks better without it

--- Scripts/orbit.gd
+++ Scripts/orbit.gd
@@ -8,10 +8,3 @@ extends Node3D
 func _physics_process(_delta):
 	rotate_y(rotation_speed)
 	
-	#camera position.z needs to grow while position.y of node grows
-func _unhandled_input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				print("growing?")
-				#position += Vector3(0, pigGrowth.growth_rate, 0)
--- Scripts/pigGrowth.gd
+++ Scripts/pigGrowth.gd
@@ -19,5 +19,5 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				scale += Vector3(growth_rate,growth_rate,growth_rate)
+				scale *= 1.02
 
